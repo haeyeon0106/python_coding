59ac6a533d0b62ec6c52ef3bcec646086c8cede8
haeyeon0106 <yeon99016@gmail.com>
2023-05-01 22:49:26 +0900

    programmers Lv.0

---
+++ 프로그래머스/Lv0/코드_처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    answer = ''
+    mode = 0
+
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] == '1' : mode = 1
+            else : 
+                if i % 2 == 0 : 
+                    answer += code[i]
+        elif mode == 1:
+            if code[i] == '1' : mode = 0
+            else : 
+                if i % 2 == 1 : 
+                    answer += code[i]
+    if answer == '' : answer = "EMPTY"
+    return answer
