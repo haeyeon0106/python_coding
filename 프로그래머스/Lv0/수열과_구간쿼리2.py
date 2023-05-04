80d2ea9477ee60d31eca156ad2297adb217b7dfd
haeyeon0106 <yeon99016@gmail.com>
2023-05-04 16:00:20 +0900

    #2 programmers Lv.0 기초 트레이닝 Day6

--- 프로그래머스/Lv0/수열과_구간쿼리2.py
+++ 프로그래머스/Lv0/수열과_구간쿼리2.py
@@ -1,16 +1,13 @@
 #정확성테스트에서 실패(수정필요)
 def solution(arr, queries):
     answer = []
-    nums = []
+    li = []
     for s,e,k in queries:
-        for i in range(s,e+1):
-            a = arr[i]
-            if a > k:
-                if a > arr[i]:
-                    a = arr[i]
-            else: a = -1
-        answer.append(a)
-        
+        if k > max(arr[s:e+1]):
+            answer.append(-1)
+        else:
+            li = [i for i in arr[s:e+1] if i > k]
+            answer.append(min(li)) if li else answer.append(-1)
     return answer
 
 print(solution([0, 1, 2, 4, 3],[[0, 4, 2],[0, 3, 2],[0, 2, 2]]))
