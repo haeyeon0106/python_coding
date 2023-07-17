772c58a2c1ebb0c707592808068cdc86da122598
haeyeon0106 <yeon99016@gmail.com>
2023-07-17 15:25:10 +0900

    programmers 고득점 kit 완전탐색 Lv.2

---
+++ 프로그래머스/고득점_kit/완전탐색/소수찾기.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+def verify(num):
+    if num == 0 or num == 1:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
+            return False     # 소수가 아님
+    return True            # 소수임
+
+def solution(numbers):
+    answer = 0
+    nums = list(map(str,numbers))
+    result = []
+    for i in range(1,len(nums)+1):
+        for j in permutations(nums,i):  # 17과 71의 순서가 중요하기 때문에 조합이 아닌 순열
+            result.append(int(''.join(j)))
+    # result = sorted(list(set(result)))
+
+    for i in list(set(result)):
+        if verify(i):
+            answer += 1
+    return answer
