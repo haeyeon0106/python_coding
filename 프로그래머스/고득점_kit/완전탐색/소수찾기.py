0a832763fb7fb0656e886fd6586c02ef504ad87f
haeyeon0106 <yeon99016@gmail.com>
2023-08-22 16:18:46 +0900

    refactoring

--- 프로그래머스/고득점_kit/완전탐색/소수찾기.py
+++ 프로그래머스/고득점_kit/완전탐색/소수찾기.py
@@ -20,3 +20,27 @@ def solution(numbers):
         if verify(i):
             answer += 1
     return answer
+
+from itertools import permutations
+def decimal(num):
+    count = 0
+    if num == 0 or num == 1:
+        return 0
+    for i in range(2,int(num*0.5)+1):
+        if num % i == 0:
+            return 0
+    return 1
+def solution2(numbers):
+    answer = 0
+    numbers = list(numbers)
+    result = []
+    total = []
+    for i in range(1,len(numbers)+1):
+        for j in permutations(numbers,i):
+            result.append(int(''.join(j)))
+
+    for i in set(result):
+        n = decimal(i)
+        total.append(n)
+        
+    return sum(total)
